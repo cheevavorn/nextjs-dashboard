3fad37f19dddf9cc87b53b4d522ee50dfd5b9d70
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2024-01-20 14:53:22 +0700

    modify: index & dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, IBM_Plex_Sans_Thai } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
+
+export const ibmPlexSansThai = IBM_Plex_Sans_Thai({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
